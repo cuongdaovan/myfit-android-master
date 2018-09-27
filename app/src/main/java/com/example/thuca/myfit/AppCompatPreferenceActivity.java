57361691d8f46250188a956a05670f3a466bfe59
cuongdaovan <cuongdaovan262@gmail.com>
2018-09-27 20:10:58 +0700

    cuong changed

--- app/src/main/java/com/example/thuca/myfit/AppCompatPreferenceActivity.java
+++ app/src/main/java/com/example/thuca/myfit/AppCompatPreferenceActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
 
 /**
  * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
